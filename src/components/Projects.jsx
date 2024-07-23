8276c6b97e78cc8cf911f92eaca802882cdf741c
Cameron Adrian <52980873+cameron-adrian@users.noreply.github.com>
2024-07-23 19:08:31 -0400

    chore: Update Projects component with new project details

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,23 @@
 import React from "react";
-// TODO: Move this out of the header into the main page maybe? Or make it a dropdown or button?
+// import { faReact } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 function Projects() {
   const projects = [
-    { id: 1, title: "Project 1", description: "Description of Project 1" },
-    { id: 2, title: "Project 2", description: "Description of Project 2" },
-    { id: 3, title: "Project 3", description: "Description of Project 3" },
+    {
+      id: 1,
+      title: "Drum Pattern Randomizer",
+      description:
+        "This app is designed to help you learn new patterns outside of the standard rudiments taught to drummers. The app presents you with a random drum pattern, including accented and ghosted notes.\n\nThis project is built with React/Vite and styled with CSS.",
+      link: "https://github.com/cameron-adrian/drum-practice-app",
+    },
+    {
+      id: 2,
+      title: "Spotify Playlist Assistant",
+      description:
+        "This app is designed to help you manage your Spotify playlists in a more granular and detailed way than you can within Spotify at the moment. You can view all of your playlists additional information about each one.\n\nThis project is built with a Django backend, styled with CSS, and uses the Spotify API.",
+      link: "https://github.com/cameron-adrian/Spotify-Playlist-Assistant",
+    },
   ];
 
   return (
@@ -14,6 +27,8 @@ function Projects() {
         <div key={project.id} className="project-card">
           <h3>{project.title}</h3>
           <p>{project.description}</p>
+          <a href={project.link}>View Project on GitHub</a>
+          {/* <FontAwesomeIcon className="icon" size="2x" icon={faReact} /> */}
         </div>
       ))}
     </section>
