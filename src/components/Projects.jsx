1d48506d634732d01fed8caf4237c5871948d15f
Cameron Adrian <52980873+cameron-adrian@users.noreply.github.com>
2024-07-24 14:34:36 -0400

    chore: Update Projects component with new project link and image

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,7 +16,11 @@ function Projects() {
       title: "Spotify Playlist Assistant",
       description:
         "This app is designed to help you manage your Spotify playlists in a more granular and detailed way than you can within Spotify at the moment. You can view all of your playlists additional information about each one.\n\nThis project is built with a Django backend, styled with CSS, and uses the Spotify API.",
-      link: "https://github.com/cameron-adrian/Spotify-Playlist-Assistant",
+      link: "https://drum-pattern-randomizer.netlify.app/",
+      githubLink:
+        "https://github.com/cameron-adrian/Spotify-Playlist-Assistant",
+      img: "/drum-pattern-randomizer.png",
+      alt: "Drum Pattern Randomizer",
     },
   ];
 
@@ -25,9 +29,14 @@ function Projects() {
       <h2>My Projects</h2>
       {projects.map((project) => (
         <div key={project.id} className="project-card">
+          <a href={project.link}>
+            <img>
+              src={project.img} alt={project.alt}
+            </img>
+          </a>
           <h3>{project.title}</h3>
           <p>{project.description}</p>
-          <a href={project.link}>View Project on GitHub</a>
+          <a>href={project.githubLink}View Project on GitHub</a>
           {/* <FontAwesomeIcon className="icon" size="2x" icon={faReact} /> */}
         </div>
       ))}
